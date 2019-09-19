b77f8aeab369e511d34ddd116131e40858fdba72
Tyler Lawson <tylerlawson12@live.com>
2019-09-19 16:35:49 -0700

    Assignment 8 complete

--- js/name.js
+++ js/name.js
@@ -1,6 +1,4 @@
 var person = prompt('Hello what is your name?')
 
-
-
 document.write('<h3>' + 'Thanks for coming ' + person + '</h3>');
 
--- js/question.js
+++ js/question.js
@@ -1,1 +1,11 @@
-var answer = confirm('Do you need training for you and your dog?')
+var answer = confirm ('Do you need training for you and your dog?')
+var response;
+
+if (answer === true) {
+    response = ('Well welcome to the right place');
+} {
+    response = ('Check out our dogs');
+}
+
+document.write('<h3>' + response + '<h3>')
+
