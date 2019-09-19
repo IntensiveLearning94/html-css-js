041f537a455d87243f1dd90bd3ece100e2f72510
Tyler Lawson <tylerlawson12@live.com>
2019-09-18 17:56:57 -0700

    lab 6 mostly done

---
+++ js/name.js
@@ -0,0 +1,5 @@
+var person = prompt('Hello what is your name?')
+
+
+
+document.write('<h3>' + 'Thanks for coming ' + person + '</h3>');
---
+++ js/question.js
@@ -0,0 +1,1 @@
+var answer = confirm('Do you need training for you and your dog?')
